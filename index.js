9e15e79f354208ea187769b02c6b8ebe8c171c31
Jochen Gererstorfer <info@j0e.org>
2021-05-15 21:25:54 +0200

    Version 2

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`Jochen Gererstorfer
+Blogger and Webdesigner - lifetime Student and Father
+
+Twitter: https://twitter.com/j0e_org
+Facebook: https://www.facebook.com/j0e.org
+WordPress: https://j0e.org/
+LMS: https://kursprofi.com/
+
+`);
